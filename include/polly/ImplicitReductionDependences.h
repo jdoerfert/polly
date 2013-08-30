28f6baa6ca5c78e881a4410a88d024eb17cf2d38
Johannes Doerfert <johannes@jdoerfert.de>
2013-08-30 18:55:11 +0200

    Renamed ReductionDependences

---
+++ include/polly/ImplicitReductionDependences.h
@@ -0,0 +1,49 @@
+//===- ImplicitReductionDependences.h - Reduction dep. analysis -*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// TODO
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef POLLY_REDUCTION_DEPENDENCES_H
+#define POLLY_REDUCTION_DEPENDENCES_H
+
+#include "polly/Dependences.h"
+
+namespace polly {
+
+class Scop;
+class ScopStmt;
+class ReductionInfo;
+
+class ImplicitReductionDependences : public Dependences {
+public:
+  static char ID;
+
+  ImplicitReductionDependences();
+
+  virtual void getAnalysisUsage(llvm::AnalysisUsage &AU) const;
+
+protected:
+  /// @brief Collect information about the SCoP.
+  virtual void collectInfo(Scop &S, isl_union_map **Read, isl_union_map **Write,
+                           isl_union_map **MayWrite, isl_union_map **Schedule);
+
+  // @brief Calculate the dependences for a certain SCoP.
+  virtual void calculateDependences(Scop &S);
+};
+
+} // End polly namespace.
+
+namespace llvm {
+class PassRegistry;
+void initializeImplicitReductionDependencesPass(llvm::PassRegistry &);
+}
+
+#endif
